61fd5887ab8ab9ef3d3a38ec9f3af6a3d6ef7929
dipankarchettri <71317043+dipankarchettri@users.noreply.github.com>
2021-12-05 13:15:06 +0530

    Update books.py

--- BOOKS/books.py
+++ BOOKS/books.py
@@ -3,31 +3,32 @@ import pandas as pd
 from tabulate import tabulate
 
 mycon = mysql.connector.connect(
-    host="localhost", user="root", password="", database="")
+    host="localhost", user="root", password="1234", database="library")
 
 if not mycon:
     print("Error in connecting")
 mycursor = mycon.cursor()
 
 
-def add_book( title,author, genre, publisher, noOfcopies):
-    #Exception Handling
+def add_book(title, author, genre, publisher, noOfcopies):
+    # Exception Handling
     try:
         mycursor.execute("insert into Bookstable(title,author,genre,publisher,noOfcopies,noofcopiesleft) values('{}','{}','{}','{}',{},{})".format(
-            title, author, genre, publisher, noOfcopies,noOfcopies))
+            title, author, genre, publisher, noOfcopies, noOfcopies))
         mycon.commit()
         return True
     except:
         return False
 
+
 def book_delete(bookID):
-    #Exception Handling
+    # Exception Handling
     try:
-        qry="select * from bookstable where bookId='{}';".format(bookID)
+        qry = "select * from bookstable where bookId='{}';".format(bookID)
         mycursor.execute(qry)
-        r=mycursor.fetchone()
+        r = mycursor.fetchone()
         if r:
-            qry="delete from bookstable where bookId='{}';".format(bookID)
+            qry = "delete from bookstable where bookId='{}';".format(bookID)
             mycursor.execute(qry)
             mycon.commit()
             return True
@@ -36,20 +37,19 @@ def book_delete(bookID):
     except:
         return False
 
+
 def book_search(bookID):
-    #Exception Handling
+    # Exception Handling
     try:
-        qry="select * from bookstable where bookId='{}';".format(bookID)
+        qry = "select * from bookstable where bookId='{}';".format(bookID)
         mycursor.execute(qry)
-        r=mycursor.fetchone()
+        r = mycursor.fetchone()
         if r:
-            qry="select * from bookstable where bookId='{}';".format(bookID)
-            df=pd.read_sql(qry,mycon)
+            qry = "select * from bookstable where bookId='{}';".format(bookID)
+            df = pd.read_sql(qry, mycon)
             print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
             return True
         else:
             return False
     except:
-        return False  
-
-
+        return False
