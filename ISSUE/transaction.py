e66fc0b82a4ef2b52de904a1d5e98fb8a1b78750
dipankarchettri <71317043+dipankarchettri@users.noreply.github.com>
2021-12-06 18:32:44 +0530

    Update transaction.py

--- ISSUE/transaction.py
+++ ISSUE/transaction.py
@@ -5,37 +5,57 @@ con = sqlt.connect(host="localhost", user="root",
 cursor = con.cursor()
 
 
+def no(id):
+    qry = "select * from usersdata where userId='{}';".format(id)
+    cursor.execute(qry)
+    r = cursor.fetchone()
+    if r:
+        nom = r[5]
+
+
 # function to issue books
+
+
 def book_issue(memID, bookID):
     # Exception Handling
     try:
         q1 = "select * from usersdata where userId={};".format(memID)
         cursor.execute(q1)
         r = cursor.fetchone()
-        if r:
-            q2 = "select bookId, NoOfCopiesLeft from bookstable where bookId={};".format(
-                bookID)
-            cursor.execute(q2)
-            r = cursor.fetchone()
-            if r:
-                if r[1] > 0:
-                    dt_str = str(datetime.datetime.now())
-                    NoOfCopiesLeft = r[1]-1
-                    q3 = "insert into issuetable(bookId,userId,DateIssued) values('{}','{}','{}');".format(
-                        bookID, memID, dt_str)
-                    cursor.execute(q3)
-                    q4 = "update bookstable set NoOfCopiesLeft={} where bookId='{}';".format(
-                        NoOfCopiesLeft, bookID)
-                    cursor.execute(q4)
-                    con.commit()
-                    return "Book Issued"
-                else:
-                    return "Book not available"
-            else:
-                return "Wrong Book ID"
+        nom = r[5]
 
+        if nom < 4:
+            nom2 = nom+1
+            if r:
+                q2 = "select bookId, NoOfCopiesLeft from bookstable where bookId={};".format(
+                    bookID)
+                cursor.execute(q2)
+                r = cursor.fetchone()
+                if r:
+                    if r[1] > 0:
+                        dt_str = str(datetime.datetime.now())
+                        NoOfCopiesLeft = r[1]-1
+                        q3 = "insert into issuetable(bookId,userId,DateIssued) values('{}','{}','{}');".format(
+                            bookID, memID, dt_str)
+                        cursor.execute(q3)
+                        q4 = "update bookstable set NoOfCopiesLeft={} where bookId='{}';".format(
+                            NoOfCopiesLeft, bookID)
+                        cursor.execute(q4)
+                        q5 = "update usersdata set booksissued={} where userid='{}';".format(
+                            nom2, memID)
+                        cursor.execute(q5)
+                        con.commit()
+
+                        return "Book Issued"
+                    else:
+                        return "Book not available"
+                else:
+                    return "Wrong Book ID"
+
+            else:
+                return "Wrong UserId"
         else:
-            return "Wrong UserId"
+            return "Maximun limit exceed"
     except:
         return "Wrong Entry"
 
@@ -43,31 +63,46 @@ def book_issue(memID, bookID):
 
 
 def book_return(memID, bookID):
-    # Exception Handliung
+    # Exception Handling
     try:
-        q1 = "select * from usersdata where userID='{}';".format(memID)
+        q1 = "select * from usersdata where userId={};".format(memID)
         cursor.execute(q1)
         r = cursor.fetchone()
-        if r:
-            q2 = "select bookId, NoOfCopiesLeft from bookstable where bookId='{}';".format(
-                bookID)
-            cursor.execute(q2)
-            r = cursor.fetchone()
-            if r:
-                dt_str = str(datetime.datetime.now())
-                remcopies = r[1]+1
-                q3 = "insert into returnTable (bookId,userId,DateReturned) values('{}','{}','{}');".format(
-                    bookID, memID, dt_str)
-                cursor.execute(q3)
-                q4 = "update booksTable set NoOfCopiesLeft={} where bookid={};".format(
-                    remcopies, bookID)
-                cursor.execute(q4)
-                con.commit()
-                return "Book Returned..."
-            else:
-                return "Wrong Book ID"
+        nom = r[5]
 
+        if nom > 0:
+            nom2 = nom-1
+            if r:
+                q2 = "select bookId, NoOfCopiesLeft from bookstable where bookId={};".format(
+                    bookID)
+                cursor.execute(q2)
+                r = cursor.fetchone()
+
+                if r:
+                    dt_str = str(datetime.datetime.now())
+                    NoOfCopiesLeft = r[1]+1
+                    q3 = "insert into issuetable(bookId,userId,DateIssued) values('{}','{}','{}');".format(
+                        bookID, memID, dt_str)
+                    cursor.execute(q3)
+                    q4 = "update bookstable set NoOfCopiesLeft={} where bookId='{}';".format(
+                        NoOfCopiesLeft, bookID)
+                    cursor.execute(q4)
+                    q5 = "update usersdata set booksissued={} where userid='{}';".format(
+                        nom2, memID)
+                    cursor.execute(q5)
+                    con.commit()
+
+                    return "Book Returned"
+
+                else:
+                    return "Wrong Book ID"
+
+            else:
+                return "Wrong UserId"
         else:
-            return "Wrong Member ID"
+            return "You dont have any books issued"
     except:
-        return "**ERROR: Wrong Entry**"
+        return "Wrong Entry"
+
+
+print(book_return("779531", "224674"))
