9c4ecccaa3ede24696a15f5718bc0b99a9e57c74
dipankarchettri <71317043+dipankarchettri@users.noreply.github.com>
2021-12-14 00:01:02 +0530

    hiiiie

--- FRONTEND/report.py
+++ FRONTEND/report.py
@@ -6,6 +6,36 @@ con = sqlt.connect(host="localhost", user="root",
                    password="1234", database="library")
 cursor = con.cursor()
 
+# function to display book details
+
+
+def book_output():
+    df = pd.read_sql("select * from bookstable", con)
+    print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+
+# function to display member details
+
+
+def member_output():
+    df = pd.read_sql("select * from usersdata", con)
+    print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+
+# function to display issue details
+
+
+def issue_output():
+    df = pd.read_sql("select * from issuetable", con)
+    print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+
+# function to display return details
+
+
+def return_output():
+    df = pd.read_sql("select * from returntable", con)
+    print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+
+# function to show the chart
+
 
 def col_chart():
     q = "select bookId,count(no_of_copies) as totalcopies from issuetable group by bookId;"
